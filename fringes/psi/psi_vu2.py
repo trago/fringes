709f20b27c00d20d364e9d7af935389501627615
Julio Cesar Estrada Rico <julio@functionize.com>
2021-01-14 09:05:55 -0600

    refactoring code

--- fringes/psi/psi_vu2.py
+++ fringes/psi/psi_vu2.py
@@ -1,5 +1,8 @@
 import numpy as np
-from typing import List, Tuple, Union
+from typing import List, Tuple, Union, Any
+
+from skimage.filters import gaussian
+from . import create_matrix
 from .psi_vu import calc_term_V, calc_term_U, print_report_info
 
 
@@ -19,7 +22,7 @@ def vu_factorization(matrix_I: np.ndarray, error_accuracy: float = 1e-3,
     """
     matrix_I += 1
     M, N = matrix_I.shape
-    deltas = np.linspace(0, np.pi, N-2, endpoint=False)
+    deltas = np.linspace(0, np.pi, N - 2, endpoint=False)
 
     if matrix_V is not None:
         matrix_U = calc_term_U(matrix_I, matrix_V)
@@ -45,7 +48,7 @@ def vu_factorization(matrix_I: np.ndarray, error_accuracy: float = 1e-3,
         matrix_U[:, 3] = np.array((0, 0, np.sin(steps[2]), np.sin(steps[3])))
         matrix_V = calc_term_V(matrix_I, matrix_U)
 
-        step = np.abs(steps[2]-steps[3])
+        step = np.abs(steps[2] - steps[3])
         error = np.abs(step - previous_phase)
         previous_phase = step
 
@@ -68,3 +71,132 @@ def calc_phase(matrix_V: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     :return: a 1D array with the phase
     """
     return np.arctan2(-matrix_V[:, 1], matrix_V[:, 2]), matrix_V[:, 0]
+
+
+def calc_phase_2steps(matrix_I, delta):
+    """
+    2-steps phase-shifting.
+    It receives a list of two interferograms and the phase-shifting value to compute its modulating phase.
+
+    :param matrix_I: A Nx2 matrix of fringe patterns. Each column is a fringe pattern
+    :param delta: the phase-shifting value
+    :return: A
+    """
+    matrix_Up = np.array([(1, 1, 1), (0, np.cos(0), np.cos(delta)), (0, np.sin(0), np.sin(delta))])
+    inv_Up = np.linalg.inv(matrix_Up)
+    matrix_Vp = matrix_I @ inv_Up
+
+    cc = matrix_Vp[:, 1]
+    ss = matrix_Vp[:, 2]
+
+    pp = np.arctan2(-ss, cc)
+    return pp
+
+
+def linear_map(image, min, max, a, b):
+    return (image - min) * (b - a) / (max - min) + a
+
+
+def vec_to_mask(mask: np.ndarray, vec_img):
+    image = np.zeros(mask.shape)
+    image[mask == 1] = vec_img
+    return image
+
+
+def create_matrix(dc, image_list, mask: np.ndarray = None):
+    if (len(image_list) == 2):
+        if isinstance(image_list, tuple):
+            image_list = dc + image_list
+        elif isinstance(image_list, list):
+            image_list = [dc[0], dc[1]] + image_list
+        else:
+            raise TypeError("Parameter image_list: A list or tuple is expected")
+    elif len(image_list) > 3:
+        image_list[0] = dc[0]
+        image_list[1] = dc[1]
+    else:
+        raise IndexError(f"Parameter image_list: len(image_list) must be >= 2, current value = {len(image_list)}")
+    if mask is None:
+        image_list = map(lambda image: image.flatten(), image_list)
+    else:
+        image_list = map(lambda image: apply_mask_to_vec(image, mask), image_list)
+    matrix = np.vstack(tuple(image_list)).T
+
+    return matrix
+
+
+def apply_mask_to_vec(image: np.ndarray, mask: np.ndarray):
+    image = image[mask == 1]
+    return image
+
+
+def img_reshape(mask: Union[Tuple[int, int], np.ndarray], img: np.ndarray) -> np.ndarray:
+    if isinstance(mask, tuple) or isinstance(mask, list):
+        return img.reshape(mask)
+    return vec_to_mask(mask, img)
+
+
+def approximate_dc(image_list, size: float = 64.0) -> Tuple[Any, ...]:
+    ones_response = gaussian(np.ones_like(image_list[0]), size, mode='constant')
+
+    dc = map(lambda image: gaussian(image, size, mode='constant') / ones_response, image_list)
+
+    return tuple(dc)
+
+
+def two_frames_demodulation(image_list: Union[List[np.ndarray], Tuple[np.ndarray]], delta, mask: np.ndarray = None,
+                            dc_kernel: float = 64, blur_kernel: float = 0.5) -> np.ndarray:
+    if len(image_list) < 2:
+        raise IndexError("The image list mist have at least two images")
+
+    images = map(lambda image: image.astype(dtype=float) if image.dtype != float else image,
+                 image_list)
+    images = tuple(images)
+    for image in image_list:
+        min = image.min()
+        max = image.max()
+        image[:, :] = linear_map(image, min, max, -1, 1)
+
+    if blur_kernel > 0:
+        images = map(lambda image: gaussian(image, blur_kernel), images)
+    images = tuple(images)
+
+    dc = approximate_dc(images, dc_kernel)
+    image_matrix = create_matrix(dc, images, mask)
+
+    return calc_phase_2steps(image_matrix, delta)
+
+
+def two_frames_vu(image_list: Union[List[np.ndarray], Tuple[np.ndarray]], mask: np.ndarray = None,
+                  dc_kernel: float = 64, blur_kernel: float = 0.5, vu_iters=50) \
+        -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    if len(image_list) < 2:
+        raise IndexError("The image list mist have at least two images")
+
+    images = map(lambda image: image.astype(dtype=float) if image.dtype != float else image,
+                 image_list)
+    images = tuple(images)
+    for image in image_list:
+        min = image.min()
+        max = image.max()
+        image[:, :] = linear_map(image, min, max, -1, 1)
+
+    if blur_kernel > 0:
+        images = map(lambda image: gaussian(image, blur_kernel), images)
+    images = tuple(images)
+
+    dc = approximate_dc(images, dc_kernel)
+    image_matrix = create_matrix(dc, images, mask)
+
+    mat_v, mat_u = vu_factorization(image_matrix, error_accuracy=1e-6, verbose=True, max_iters=vu_iters)
+    pp = np.arctan2(-mat_v[:, 3], mat_v[:, 2])
+    steps = np.arctan2(-mat_u[:, 3], mat_u[:, 2])
+
+    if mask is None:
+        shape = image_list[0].shape
+        return pp.reshape(shape), steps[2:], mat_v[:, 0].reshape(shape), mat_v[:, 1].reshape(shape)
+    else:
+        pp = img_reshape(mask, pp)
+        dc0 = img_reshape(mask, mat_v[:, 0])
+        dc1 = img_reshape(mask, mat_v[:, 1])
+        return pp, steps[2:], dc0, dc1
--- tests/psi/psi_2steps.py
+++ tests/psi/psi_2steps.py
@@ -4,112 +4,9 @@ Numerical simulation for testing the PSI-VU factorization.
 
 import numpy as np
 from matplotlib import pylab as plt
-from fringes.psi import *
+from fringes.psi.psi_vu2 import two_frames_vu
 from fringes.simulations import functions
-from skimage.filters import gaussian
 import skimage.io as ski_io
-from fringes.psi import psi_vu2
-from typing import Union, List, Tuple
-
-
-def linear_map(image, min, max, a, b):
-    return (image - min) * (b - a) / (max - min) + a
-
-
-def apply_mask_to_vec(image: np.ndarray, mask: np.ndarray):
-    image = image[mask == 1]
-    return image
-
-
-def vec_to_mask(mask: np.ndarray, vec_img):
-    image = np.zeros(mask.shape)
-    image[mask == 1] = vec_img
-    return image
-
-
-def create_matrix(dc, image_list, mask: np.ndarray = None):
-    if (len(image_list) == 2):
-        if isinstance(image_list, tuple):
-            image_list = dc + image_list
-        elif isinstance(image_list, list):
-            image_list = [dc[0], dc[1]] + image_list
-        else:
-            raise TypeError("Parameter image_list: A list or tuple is expected")
-    elif len(image_list) > 3:
-        image_list[0] = dc[0]
-        image_list[1] = dc[1]
-    else:
-        raise IndexError(f"Parameter image_list: len(image_list) must be >= 2, current value = {len(image_list)}")
-    if mask is None:
-        image_list = map(lambda image: image.flatten(), image_list)
-    else:
-        image_list = map(lambda image: apply_mask_to_vec(image, mask), image_list)
-    matrix = np.vstack(tuple(image_list)).T
-
-    return matrix
-
-
-def img_reshape(mask: Union[Tuple[int, int], np.ndarray], img: np.ndarray) -> np.ndarray:
-    if isinstance(mask, tuple) or isinstance(mask, list):
-        return img.reshape(mask)
-    return vec_to_mask(mask, img)
-
-
-def calc_phase(matrix_I, delta):
-    matrix_Up = np.array([(1, 1, 1), (0, np.cos(0), np.cos(delta)), (0, np.sin(0), np.sin(delta))])
-    inv_Up = np.linalg.inv(matrix_Up)
-    matrix_Vp = matrix_I @ inv_Up
-
-    cc = matrix_Vp[:, 1]
-    ss = matrix_Vp[:, 2]
-
-    pp = np.arctan2(-ss, cc)
-    magn = np.sqrt(ss ** 2 + cc ** 2)
-    return magn * np.cos(pp), magn * np.sin(pp), pp
-
-
-def approximate_dc(image_list, size=64) -> Tuple[np.ndarray, np.ndarray]:
-    ones_response = gaussian(np.ones_like(image_list[0]), size, mode='constant')
-
-    dc = map(lambda image: gaussian(image, size, mode='constant')/ones_response, image_list)
-
-    return tuple(dc)
-
-
-def two_frames_phase(image_list: Union[List[np.ndarray], Tuple[np.ndarray]], mask: np.ndarray = None,
-                     dc_kernel: float = 64, blur_kernel: float = 0.5, vu_iters=50) \
-        -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
-    if len(image_list) < 2:
-        raise IndexError("The image list mist have at least two images")
-
-    images = map(lambda image: image.astype(dtype=float) if image.dtype != float else image,
-                 image_list)
-    images = tuple(images)
-    for image in image_list:
-        min = image.min()
-        max = image.max()
-        image[:, :] = linear_map(image, min, max, -1, 1)
-
-    if blur_kernel > 0:
-        images = map(lambda image: gaussian(image, blur_kernel), images)
-    images = tuple(images)
-
-    dc = approximate_dc(images, dc_kernel)
-    image_matrix = create_matrix(dc, images, mask)
-
-    mat_v, mat_u = psi_vu2.vu_factorization(image_matrix, error_accuracy=1e-6, verbose=True, max_iters=vu_iters)
-    pp = np.arctan2(-mat_v[:, 3], mat_v[:, 2])
-    steps = np.arctan2(-mat_u[:, 3], mat_u[:, 2])
-
-    if mask is None:
-        shape = image_list[0].shape
-        return pp.reshape(shape), steps[2:], mat_v[:, 0].reshape(shape), mat_v[:, 1].reshape(shape)
-    else:
-        pp = img_reshape(mask, pp)
-        dc0 = img_reshape(mask, mat_v[:, 0])
-        dc1 = img_reshape(mask, mat_v[:, 1])
-        return pp, steps[2:], dc0, dc1
-
 
 # Number of fringe patterns
 K = 3
@@ -133,7 +30,7 @@ I1 = ski_io.imread('../data/acetato_1.png', as_gray=True)
 I2 = ski_io.imread('../data/acetato_2.png', as_gray=True) #+ functions.gaussian(150, I1.shape)
 mask = ski_io.imread('../data/acetato_mask.png', as_gray=True)
 image_list = (I1, I2)
-pp, steps, dc0, dc1 = two_frames_phase(image_list, mask=mask, dc_kernel=32, blur_kernel=.3, vu_iters=100)
+pp, steps, dc0, dc1 = two_frames_vu(image_list, mask=mask, dc_kernel=32, blur_kernel=.3, vu_iters=100)
 
 cc = np.cos(pp)
 ss = np.sin(pp)
